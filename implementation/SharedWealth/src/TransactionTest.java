b58565a8faedd46f5213702c5667abca45ec06a2
coder121 <siddiq235@gmail.com>
2015-08-30 12:20:46 -0700

    fixed untracked files

--- implementation/SharedWealth/src/TransactionTest.java
+++ implementation/SharedWealth/src/TransactionTest.java
@@ -1,17 +1,14 @@
-import static org.junit.Assert.*;
-
 import org.junit.Assert;
 import org.junit.Test;
-
 
 public class TransactionTest {
 
 	@Test
 	public void setTransactionTest() {
-		
-		Transaction t=new Transaction("aras", 1);
-		Assert.assertEquals(1.0, t.getAmount(),0);
-	
+
+		Transaction t = new Transaction("aras", 1);
+		Assert.assertEquals(1.0, t.getAmount(), 0);
+
 	}
 
 }
