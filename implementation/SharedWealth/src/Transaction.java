9fb7ffaa5b3d0213cede18459a0c2e447624eaa1
coder121 <siddiq235@gmail.com>
2015-08-29 17:08:11 -0700

    implementation in java

---
+++ implementation/SharedWealth/src/Transaction.java
@@ -0,0 +1,40 @@
+
+public class Transaction {
+	  private static final int version=1;
+	    private int listofInput;
+	    private int listofOutputs;
+	    private String senderPk;
+	    private String receiverPk;
+	    private double amount;
+
+
+	    public Transaction( String receiverPK,double amount) {
+	        this.receiverPk=receiverPK;
+	        this.amount=amount;
+	        this.senderPk="akjsak";
+	        this.listofInput=1;
+	        this.listofOutputs=1;
+	    }
+	    public String getSenderPk(){
+	        return senderPk;
+	    }
+	    public String getReceiverPk(){
+	        return receiverPk;
+	    }
+	    public double getAmount(){
+	        return amount;
+	    }
+	    public int getListofInput(){
+	        return listofInput;
+	    }
+	    public int getListofOutput(){
+	        return listofOutputs;
+	    }
+	    public void setAmount(double Amount){
+	        this.amount=amount;
+	    }
+	    public void setInputs(){
+	        this.listofInput=listofInput;
+	    }
+
+}
---
+++ implementation/SharedWealth/src/TransactionTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TransactionTest {
+
+	@Test
+	public void setTransactionTest() {
+		
+		Transaction t=new Transaction("aras", 1);
+		Assert.assertEquals(1.0, t.getAmount(),0);
+	
+	}
+
+}
