6a3fcbc0b3092598367854c6be1b325209bea7ac
coder121 <siddiq235@gmail.com>
2015-09-16 22:09:15 -0700

    added sha256 double hash

--- implementation/SharedWealth/src/Util.java
+++ implementation/SharedWealth/src/Util.java
@@ -35,4 +35,29 @@ public static String getHash(String text) {
 	}
 	return hash;
 }
+public static String getDoubleHash(String text) {
+	MessageDigest md;
+	MessageDigest md2;
+	byte[] digest = null;
+	byte[] finalDigest = null;
+	String hash=null;
+	try {
+		md = MessageDigest.getInstance("SHA-256");
+		md2=MessageDigest.getInstance("SHA-256");
+		md.update(text.getBytes("UTF-8")); // Change this to "UTF-16" if needed
+		digest = md.digest();
+		md2.update(digest);
+		finalDigest=md2.digest();
+		 BigInteger bi = new BigInteger(1, finalDigest);
+		   hash = (String.format("%0" + (finalDigest.length << 1) + "X", bi)).toLowerCase(); //converting bytes to string
+	} catch (NoSuchAlgorithmException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (UnsupportedEncodingException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	return hash;
+}
+
 }
